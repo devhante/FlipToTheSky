9f7b12ac3ae13408605c47ae14ca4df3490b8295
Yujun Cho <devhante@gmail.com>
2022-07-05 19:52:56 +0900

    레벨 바닥 타일 스프라이트 변경
    1. 레벨 바닥 스프라이트 변경
    2. 레벨 크기 32->30타일로 변경
    3. 일시정지 버튼 안눌리는 버그 해결

--- Assets/Scripts/PlayScene/LevelController.cs
+++ Assets/Scripts/PlayScene/LevelController.cs
@@ -12,16 +12,16 @@ namespace FTS.PlayScene
 
         private void Start()
         {
-            Instantiate(levels[Random.Range(0, levels.Length)], new Vector3(32, 0, 0), Quaternion.identity);
-            Instantiate(levels[Random.Range(0, levels.Length)], new Vector3(64, 0, 0), Quaternion.identity);
-            lastLevel = Instantiate(levels[Random.Range(0, levels.Length)], new Vector3(96, 0, 0), Quaternion.identity);
+            Instantiate(levels[Random.Range(0, levels.Length)], new Vector3(30, 0, 0), Quaternion.identity);
+            Instantiate(levels[Random.Range(0, levels.Length)], new Vector3(60, 0, 0), Quaternion.identity);
+            lastLevel = Instantiate(levels[Random.Range(0, levels.Length)], new Vector3(90, 0, 0), Quaternion.identity);
         }
 
         private void LateUpdate()
         {
-            if (lastLevel.transform.position.x <= 64)
+            if (lastLevel.transform.position.x <= 60)
             {
-                lastLevel = Instantiate(levels[Random.Range(0, levels.Length)], new Vector3(lastLevel.transform.position.x + 32, 0, 0), Quaternion.identity);
+                lastLevel = Instantiate(levels[Random.Range(0, levels.Length)], new Vector3(lastLevel.transform.position.x + 30, 0, 0), Quaternion.identity);
             }
         }
     }
