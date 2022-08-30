e55be5a0414fb6f4fbcedf59daedb1a4a16e90fb
Yujun Cho <devhante@gmail.com>
2022-08-30 17:38:18 +0900

    조이스틱 구현

--- Assets/Scripts/PlayScene/UI/Joystick.cs
+++ Assets/Scripts/PlayScene/UI/Joystick.cs
@@ -5,41 +5,42 @@ using UnityEngine.EventSystems;
 
 namespace FTS.PlayScene
 {
-    public class Joystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    public class Joystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
     {
         public Vector3 Normal { get; private set; }
 
-        private Transform handle;
+        private RectTransform rt;
+        private RectTransform handleRT;
+        private Canvas canvas;
         private float radius;
-        private int xAxis;
-        private int yAxis;
+        private Vector2 mousePos;
 
         private void Awake()
         {
-            handle = transform.GetChild(0);
-            radius = GetComponent<RectTransform>().sizeDelta.x * 0.5f;
+            rt = GetComponent<RectTransform>();
+            handleRT = transform.GetChild(0).GetComponent<RectTransform>();
+            canvas = transform.parent.GetComponent<Canvas>();
+            radius = rt.sizeDelta.x * 0.5f;
         }
 
-        private void Start()
-        {
-            StartCoroutine(ChangeControllerPositionCoroutine());
-        }
-
-        private IEnumerator ChangeControllerPositionCoroutine()
-        {
-            yield return null;
-        }
 
         public void OnPointerDown(PointerEventData eventData)
         {
-            StartCoroutine("PointerDown");
+            OnDrag(eventData);
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            mousePos = (eventData.position - rt.anchoredPosition) / (radius * canvas.scaleFactor);
+            if (mousePos.magnitude > 1) mousePos = mousePos.normalized;
+            if (mousePos.magnitude <= 0) mousePos = Vector2.zero;
+            handleRT.anchoredPosition = mousePos * radius;
         }
 
         public void OnPointerUp(PointerEventData eventData)
         {
-            StopCoroutine("PointerDown");
-            handle.transform.position = transform.position;
-            Normal = Vector3.zero;
+            mousePos = Vector2.zero;
+            handleRT.anchoredPosition = Vector2.zero;
         }
     }
 }
